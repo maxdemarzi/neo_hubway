6833f2c848dcca96b677c5ce7c0b6cc78ebcfe85
Max De Marzi <maxdemarzi@hotmail.com>
2012-10-11 15:39:21 -0500

    show index.html on root

--- neo_hubway.rb
+++ neo_hubway.rb
@@ -17,3 +17,7 @@ get '/visualization' do
   results = stations
   results.group_by(&:first).map {|k,v| {"name" => k, "follows" => v.collect{|n| { "name" => n[1], "counts" => n[2] }}}}.to_json
 end
+
+get '/' do
+  File.read(File.join('public', 'index.html'))
+end
